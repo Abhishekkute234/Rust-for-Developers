5e08ee7bc7796f74b94b65235fbd1475f32fbc0f
Abhishekkute234 <kuteabhishek2004@gmail.com>
2024-10-30 18:41:49 +0530

    create the 1st project in the rust

---
+++ project_name/target/debug/deps/project_name-a5cfe9dd4071e89b.d
@@ -0,0 +1,5 @@
+/home/abhishek/Documents/Rust-for-Developers/project_name/target/debug/deps/project_name-a5cfe9dd4071e89b: src/main.rs
+
+/home/abhishek/Documents/Rust-for-Developers/project_name/target/debug/deps/project_name-a5cfe9dd4071e89b.d: src/main.rs
+
+src/main.rs:
---
+++ project_name/target/debug/project_name.d
@@ -0,0 +1,1 @@
+/home/abhishek/Documents/Rust-for-Developers/project_name/target/debug/project_name: /home/abhishek/Documents/Rust-for-Developers/project_name/src/main.rs
